c131ed015518671d82180221a0b1db22868d05e4
Rad Cirskis <nad2000@gmail.com>
2020-10-26 14:00:08 +1300

    added mac to address printing

---
+++ SHM/demo/writer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+extern int create_and_write_shared_memory(
+	char *mmap_key, 
+	char *value, 
+	unsigned int size);
+
+int main (int argc, char **argv) {
+
+    char *key = "/introduction";
+    char *intro = "Hello, i am Abhishek, Udemy instructor, and i love my students";
+    if (argc > 1) {
+	    intro = argv[1];
+    }
+    create_and_write_shared_memory(key, intro, strlen(intro));
+    return 0;
+}
