5cfd35e6f4192ae359f5fb7e727e5ba364cf01d7
Rad Cirskis <nad2000@gmail.com>
2021-04-05 23:10:26 +1200

    ignore git hooks

--- update_gitignore.sh
+++ update_gitignore.sh
@@ -1,5 +1,5 @@
 #!/bin/env sh
 
 # add executales to the .gitignore
-for f in $(find . -perm /111 -type f | sed 's#^./##' | sort -u); do grep -q "$f" .gitignore || echo "$f" >> .gitignore ; done
+for f in $(find . -perm /111 -type f | grep -v '.git' | sed 's#^./##' | sort -u); do grep -q "$f" .gitignore || echo "$f" >> .gitignore ; done
 
